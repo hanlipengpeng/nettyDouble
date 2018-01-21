797adca9eb429dc64a7f28c29570fb3277903354
hanlipengpeng <915619712@qq.com>
2018-01-21 21:53:24 +0800

    jvm

---
+++ nettyDouble/src/main/java/com/zzrenfeng/jvm/demo/WhatJVM.java
@@ -0,0 +1,13 @@
+package com.zzrenfeng.jvm.demo;
+/**
+ * JVM 的垃圾回收
+ * 需要学习的有：
+ * 1：都有哪些垃圾回收的算法
+ * 2：都有哪些内存，主要学习堆内存的一些垃圾回收，和里面的分类
+ * 3：学习使用jvm的调试工具，能够看出内存的使用，cpu的使用，gc等情况。
+ * @author hanlipeng
+ *
+ */
+public class WhatJVM {
+
+}
